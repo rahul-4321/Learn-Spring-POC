e905f3c8983e285040c6b0464abf20802a371b04
rahul-4321m <shahrahul2051@gmail.com>
2025-05-21 23:23:23 +0530

    using @Autowired annotation to autowire the dependencies, HERE- Laptop dependency is autowired by the Spring

---
+++ spring framework/springframework/src/main/java/com/rahul/springframework/Laptop.java
@@ -0,0 +1,10 @@
+package com.rahul.springframework;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void show(){
+        System.out.println("Laptop class");
+    }
+}
